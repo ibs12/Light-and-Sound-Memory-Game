a29531f6aea16db9305285d351a43f060331b062
ibs12 <ibrahimallahb@gmail.com>
2022-04-01 23:58:13 -0400

    Completed Pre-Work (might update)

--- README.md
+++ README.md
@@ -4,9 +4,10 @@
 
 Submitted by: Ibrahim Allahbukhsh
 
-Time spent: 5 hours spent in total
+Time spent: Over 40 hours spent in total
 
-Link to project: (insert your link here, should start with https://glitch.com...)
+Link to project: https://codepath-prework---ibrahim-allahbukhsh.glitch.me (live site)
+                 https://glitch.com/edit/#!/codepath-prework---ibrahim-allahbukhsh (code)
 
 ## Required Functionality
 
@@ -22,51 +23,55 @@ The following **required** functionality is complete:
 
 The following **optional** features are implemented:
 
-* [ ] Any HTML page elements (including game buttons) has been styled differently than in the tutorial
-* [ ] Buttons use a pitch (frequency) other than the ones in the tutorial
-* [ ] More than 4 functional game buttons
-* [ ] Playback speeds up on each turn
-* [ ] Computer picks a different pattern each time the game is played
-* [ ] Player only loses after 3 mistakes (instead of on the first mistake)
-* [ ] Game button appearance change goes beyond color (e.g. add an image)
-* [ ] Game button sound is more complex than a single tone (e.g. an audio file, a chord, a sequence of multiple tones)
-* [ ] User has a limited amount of time to enter their guess on each turn
+* [X] Any HTML page elements (including game buttons) has been styled differently than in the tutorial
+* [X] Buttons use a pitch (frequency) other than the ones in the tutorial
+* [X] More than 4 functional game buttons
+* [X] Playback speeds up on each turn
+* [X] Computer picks a different pattern each time the game is played
+* [X] Player only loses after 3 mistakes (instead of on the first mistake)
+* [X] Game button appearance change goes beyond color (e.g. add an image)
+* [X] Game button sound is more complex than a single tone (e.g. an audio file, a chord, a sequence of multiple tones)
+* [X] User has a limited amount of time to enter their guess on each turn
 
 The following **additional** features are implemented:
 
-- [ ] List anything else that you can get done to improve the app!
+Created doNotCheat function so people are unable to cheat and select buttons as the computer is giving out the clues and the timer is not running
+Allowed users to set there own deired length of the pattern
+
+- [Diffent levels of difficulty with short times and clue sequences being played faster, multiples sounds for each button, Create a musical mode where it just plays random instruments at the same time since all of my buttons already play instruments it wouldn't be too hard to add a few more button, ] List anything else that you can get done to improve the app!
 
 ## Video Walkthrough (GIF)
 
 If you recorded multiple GIFs for all the implemented features, you can add them here:
-![](gif1-link-here)
-![](gif2-link-here)
-![](gif3-link-here)
-![](gif4-link-here)
+![](http://g.recordit.co/wnbMJvkAN9.gif)First win showcase
+![](http://g.recordit.co/45Tn4PL0pU.gif)Shows Strikes allowing 2 mistakes, but on the third one you lose
+![](http://g.recordit.co/FckcXx6wtt.gif)Shows randomly generated patterns each time the game starts, clueHoldTime decreaseing each turn and showing user changing pattern length
+![](http://g.recordit.co/Rq0sIB65C0.gif)Shows User selecting a button before the timer start which causes him to lose the game 
+![](http://g.recordit.co/WXn42Yk9Rx.gif)Shows user running out of time and losing the game 
 
-## Reflection Questions
+## Reflection Questionss
 1. If you used any outside resources to help complete your submission (websites, books, people, etc) list them here. 
-[YOUR ANSWER HERE]
+[programminghead.com, ￼w3schools.com, mozilla.org, GeeksforGeeks.org]
 
 2. What was a challenge you encountered in creating this submission (be specific)? How did you overcome it? (recommended 200 - 400 words) 
-[YOUR ANSWER HERE]
+[I had many challenges particularly with completing a lot of the optional features. The ticking clock feature gave me the hardest time. Because you need to know when to start, when to restart and especially you need to know how to update the timer and make a visual clock the user can see. Well one of the problems I had was that I was calling the timer when you press the start button, but with a little bit of a delay because we have to start the time when the computer is done giving our the clues. And so if you pressed the start button and right after press the stop button before the startTimer function has the chance to be called. So that means the stopTimer is called to stop the time, but the times hasn't started yet. So then after the delay the timer start even though the game is not playing. This problem really got me stuck for a good hour or two even though in the end it was pretty obvious. Why not just make it so the time only runs when the gamePlaying variable is true. Even though it was simple it made made go through my code a different eay and think out the box. Was able to solve a lot of other problems I was facing after that. For the smaller challenges I would use console.log a lot to get a good understanding of what my code is doing so far. ]
 
 3. What questions about web development do you have after completing your submission? (recommended 100 - 300 words) 
-[YOUR ANSWER HERE]
+[After completing my submission i've realized that all it takes is some practice/experimentation because with HTML/JS/CSS you can directly see the result your code and you can play around with it until you understand how it works. But I feel as though with just using HTML/JS/CSS you're thers's so many thing we can do, but it take a long time and tons of lines of code.  I know there is a lot more to web development than HTML/JS/CSS that I have yet to actually explore. And there is so much more you can do in web development. So what would you say is your favorite topic from web development that's not HTML/JS/CSS that makes web development more exciting? I know during this program we explore a lot of these other topics, throughout the summer, but I would like to get a heads up.  ]
 
 4. If you had a few more hours to work on this project, what would you spend them doing (for example: refactoring certain functions, adding additional features, etc). Be specific. (recommended 100 - 300 words) 
-[YOUR ANSWER HERE]
+[I would add more features, to make the game look more user friendly. Particulary with animations, i've tried to do it in this project, but it was gonna take too long for me to figure it out. I've already spent way too many hours trying to figure out how everything works for things I don't really need for the project. I would like to make it as smooth as possble, if I knew how bcause sometimes It can feel clunky. I would add more buttons as instruments to play all in a certain random pattern to make some type of beat or music as a fun mode. But right now I feel as though since we are justing starting with the beginner level topics we don't really realize the full potential of what we can do with web development with certain topic we'll learn over the summer. ]
 
 
 
 ## Interview Recording URL Link
 
-[My 5-minute Interview Recording](your-link-here)
+[My 5-minute Interview Recording](https://buffalo.zoom.us/rec/share/zRuPoQjJlU-y17HkhLWGOWtzUgQahm7NxtHcgkk-79vufC179G-aTBpl1TOVBHcO.uTI7g1qBpIe4a3Cs?startTime=1648785074000)
 
 
 ## License
 
-    Copyright [YOUR NAME]
+    Copyright [Ibrahim Allahbukhsh]
 
     Licensed under the Apache License, Version 2.0 (the "License");
     you may not use this file except in compliance with the License.
--- prework/script.js
+++ prework/script.js
@@ -1,62 +1,185 @@
 //Global Constants
-const clueHoldTime = 1000; //how long to hold each clue's light/sound
-const cluePauseTime = 333; //how long to pause in between clues
-const nextClueWaitTime = 1000; //how long to wait  before playing sequence
+const TIME_LIMIT = 20;
+const CLUE_PAUSE_TIME = 500; //how long to pause in between clues
+const NEXT_CLUE_WAIT_TIME = 1000; //how long to wait  before playing sequence
 
 //Global Variables
-
-var pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+var clueHoldTime = 3500; //how long to hold each clue's light/sound
+var pattern = [];
+var randomNumber = 0;
 var progress = 0;
 var gamePlaying = false;
 var tonePlaying = false;
+var timerRunning = false;
 var volume = 0.5;
 var guessCounter = 0;
+var strikes = 0;
+var timePassed = 0;
+var timeLeft = TIME_LIMIT;
+var timerInterval = null;
+var lengthOfPattern = 10;
+
+//Set Strikes and Time to default values
+document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
+document.getElementById("timerFormat").innerHTML =
+  "Time: " + formatTime(timeLeft);
+
+//Gets value from user textbox to keep track of the length the user wants to set
+function getLengthOfPattern() {
+  lengthOfPattern = document.getElementById("patternlength").value;
+  document.getElementById("currentLength").innerHTML =
+    "Current Length: " + lengthOfPattern;
+
+  console.log(lengthOfPattern);
+}
+
+/* If user selects a clue as the computer gives the clues
+before the timer starts they automatically lose! */
+function doNotCheat() {
+  if (timerRunning == false && gamePlaying == true) {
+    cheaterLoseGame();
+  }
+}
+
+// --------**TIMER FUNCTIONS**--------
+function stopTimer() {
+  clearInterval(timerInterval);
+}
+
+function startTimer() {
+  if (gamePlaying == true) {
+    timerRunning = true;
+
+    document.getElementById("screenDirection").innerHTML =
+      "Repeat the pattern!";
+
+    timerInterval = setInterval(function () {
+      timePassed = timePassed += 1; //Keeptrack of time passed
+      timeLeft = TIME_LIMIT - timePassed;
+      document.getElementById("timerFormat").innerHTML =
+        "Time: " + formatTime(timeLeft);
+
+      if (timeLeft < 0) {
+        loseGame();
+      }
+    }, 1000);
+  }
+}
+function resetTimer() {
+  stopTimer();
+  timerInterval = null;
+  timePassed = 0;
+  timeLeft = TIME_LIMIT;
+  document.getElementById("timerFormat").innerHTML =
+    "Time: " + formatTime(timeLeft);
+  document.getElementById("screenDirection").innerHTML = "";
+
+  timerRunning = false;
+}
+
+function formatTime(timeLeft) {
+  var seconds = timeLeft % 60;
+
+  if (seconds < 10) {
+    seconds = `0${seconds}`;
+  }
+
+  return `${0}:${seconds}`;
+}
+// --------**RANDOM SEQUENCE GENERATOR**--------
+function getRandomPattern(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  for (let j = 0; j < lengthOfPattern; j++) {
+    var randomNumber = Math.floor(Math.random() * (max - min + 1) + min);
+    pattern.push(randomNumber);
+  }
+  console.log(pattern);
+}
+
+// --------**START/STOP GAME FUNCTIONS**--------
 
 function startGame() {
   //initialize game variables
+  strikes = 0;
+  clueHoldTime = 3500;
   progress = 0;
   gamePlaying = true;
   //swap the Start and Stop buttons
+  pattern = [];
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
+  getRandomPattern(1, 6);
   playClueSequence();
 }
 
 function stopGame() {
   gamePlaying = false;
+  strikes = 0;
+
   //swap the Start and Stop buttons
+  document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
+  document.getElementById("screenDirection").innerHTML = "";
+
+  for (let l = 1; l < 7; l++) {
+    removeImage(l);
+    pauseMyAudio(l);
+    clearButton(l);
+  }
+
+  resetTimer();
 }
 
+// --------**LIGHT BUTTON WHEN PLAYING FUNCTION**--------
 function lightButton(btn) {
   document.getElementById("button" + btn).classList.add("lit");
 }
+
+// --------**STOP ALL VISUAL AND AUDIO CLUES FUNCTION**--------
+
 function clearButton(btn) {
   document.getElementById("button" + btn).classList.remove("lit");
+  pauseMyAudio(btn);
+  removeImage(btn);
 }
+
+// --------**PLAY CLUES FUNCTIONS**--------
 
 function playSingleClue(btn) {
   if (gamePlaying) {
     lightButton(btn);
-    playTone(btn, clueHoldTime);
+    playMyAudio(btn, clueHoldTime);
     setTimeout(clearButton, clueHoldTime, btn);
+    displayImage(btn);
   }
 }
 
 function playClueSequence() {
   guessCounter = 0;
   context.resume();
-  let delay = nextClueWaitTime; //set delay to initial wait time
+  let delay = NEXT_CLUE_WAIT_TIME; //set delay to initial wait time
   for (let i = 0; i <= progress; i++) {
     // for each clue that is revealed so far
+
+    clueHoldTime -= 50;
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
     setTimeout(playSingleClue, delay, pattern[i]); // set a timeout to play that clue
     delay += clueHoldTime;
-    delay += cluePauseTime;
+    delay += CLUE_PAUSE_TIME;
+    console.log("clueHoldTime: " + clueHoldTime);
   }
-}
 
+  setTimeout(startTimer, delay);
+}
+// --------**STOP GAME FUNCTIONS**--------
+function cheaterLoseGame() {
+  stopGame();
+  alert(
+    "Game Over. You lost. Please wait until the timer starts to repeat the pattern. "
+  );
+}
 function loseGame() {
   stopGame();
   alert("Game Over. You lost. 😢");
@@ -65,6 +188,8 @@ function winGame() {
   stopGame();
   alert("Game Over. You won! 😃");
 }
+
+// --------**GAME LOGIC FUNCTION**--------
 function guess(btn) {
   console.log("user guessed: " + btn);
 
@@ -82,24 +207,141 @@ function guess(btn) {
         //Pattern correct. Add next segment
         progress++;
         playClueSequence();
+        resetTimer();
       }
     } else {
       //so far so good... check the next guess
       guessCounter++;
     }
   } else {
+    strikes++;
+    document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
+  }
+  if (strikes == 3) {
+    document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
     //Guess was incorrect
     //GAME OVER: LOSE!
     loseGame();
   }
 }
 
+// --------**IMAGE FUNCTIONS**--------
+
+function displayImage(btn) {
+  var element;
+  if (btn == 1) {
+    element = document.getElementById("guitarImage");
+  }
+  if (btn == 2) {
+    element = document.getElementById("drumImage");
+  }
+  if (btn == 3) {
+    element = document.getElementById("pianoImage");
+  }
+  if (btn == 4) {
+    element = document.getElementById("violinImage");
+  }
+  if (btn == 5) {
+    element = document.getElementById("saxophoneImage");
+  }
+  if (btn == 6) {
+    element = document.getElementById("trumpetImage");
+  }
+
+  element.classList.remove("hiddenImage");
+  element.style.display = "block";
+}
+
+function removeImage(btn) {
+  var element;
+  if (btn == 1) {
+    element = document.getElementById("guitarImage");
+  }
+  if (btn == 2) {
+    element = document.getElementById("drumImage");
+  }
+  if (btn == 3) {
+    element = document.getElementById("pianoImage");
+  }
+  if (btn == 4) {
+    element = document.getElementById("violinImage");
+  }
+  if (btn == 5) {
+    element = document.getElementById("saxophoneImage");
+  }
+  if (btn == 6) {
+    element = document.getElementById("trumpetImage");
+  }
+
+  element.classList.add("hiddenImage");
+}
+
+// --------**AUDIO FUNCTIONS**--------
+
+function playMyAudio(btn, len) {
+  var audio;
+  if (btn == 1) {
+    audio = document.getElementById("guitarAudio");
+  }
+  if (btn == 2) {
+    audio = document.getElementById("drumsAudio");
+  }
+  if (btn == 3) {
+    audio = document.getElementById("pianoAudio");
+  }
+  if (btn == 4) {
+    audio = document.getElementById("violinAudio");
+  }
+  if (btn == 5) {
+    audio = document.getElementById("saxophoneAudio");
+  }
+  if (btn == 6) {
+    audio = document.getElementById("trumpetAudio");
+  }
+
+  if (audio.paused) {
+    audio.currentTime = 0;
+    audio.play();
+  } else {
+    audio.play();
+  }
+  setTimeout(function () {
+    stopTone();
+  }, len);
+}
+
+function pauseMyAudio(btn) {
+  var pauseAudio;
+  if (btn == 1) {
+    pauseAudio = document.getElementById("guitarAudio");
+  }
+  if (btn == 2) {
+    pauseAudio = document.getElementById("drumsAudio");
+  }
+  if (btn == 3) {
+    pauseAudio = document.getElementById("pianoAudio");
+  }
+  if (btn == 4) {
+    pauseAudio = document.getElementById("violinAudio");
+  }
+  if (btn == 5) {
+    pauseAudio = document.getElementById("saxophoneAudio");
+  }
+  if (btn == 6) {
+    pauseAudio = document.getElementById("trumpetAudio");
+  }
+
+  pauseAudio.pause();
+}
+
 // Sound Synthesis Functions
-const freqMap = {
+/*const freqMap = {
   1: 261.6,
   2: 329.6,
   3: 392,
   4: 466.2,
+  5: 496.7,
+  6: 596.5,
 };
 function playTone(btn, len) {
   o.frequency.value = freqMap[btn];
@@ -118,7 +360,7 @@ function startTone(btn) {
     context.resume();
     tonePlaying = true;
   }
-}
+}*/
 function stopTone() {
   g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
   tonePlaying = false;
