40222c3e87e94c9d159fac21b4cc1d505cd97d2f
ibs12 <ibrahimallahb@gmail.com>
2022-04-23 02:43:42 -0400

    Pre-Work
    Updated game with more customization for the user to play their own way.

--- prework/script.js
+++ prework/script.js
@@ -1,10 +1,8 @@
 //Global Constants
-const TIME_LIMIT = 20;
 const CLUE_PAUSE_TIME = 500; //how long to pause in between clues
 const NEXT_CLUE_WAIT_TIME = 1000; //how long to wait  before playing sequence
 
 //Global Variables
-var clueHoldTime = 3500; //how long to hold each clue's light/sound
 var pattern = [];
 var randomNumber = 0;
 var progress = 0;
@@ -13,26 +11,157 @@ var tonePlaying = false;
 var timerRunning = false;
 var volume = 0.5;
 var guessCounter = 0;
-var strikes = 0;
+var timeLimit = 20;
 var timePassed = 0;
-var timeLeft = TIME_LIMIT;
+var timeLeft = timeLimit;
 var timerInterval = null;
 var lengthOfPattern = 10;
+var clueHoldTime = 3500; //how long to hold each clue's light/sound
+var displayClueHoldTime = 3.5;
+var changeHoldTimeBy = -50;
+var displayChangeHoldTimeBy = -0.05;
+var strikes = 0;
+var strikeLimit = 3;
 
-//Set Strikes and Time to default values
+//Set Strikes, Time, Clue length and +/- Clue Length to default values
+
 document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
-document.getElementById("timerFormat").innerHTML =
-  "Time: " + formatTime(timeLeft);
+document.getElementById("timerFormat").innerHTML = formatTime(timeLeft);
+document.getElementById("clueLengthHoldTime+/-").innerHTML =
+  "+/- Clue Time Each Turn: " + displayChangeHoldTimeBy + " second(s)";
+document.getElementById("currentHoldTime").innerHTML =
+  "Clue Time: " + displayClueHoldTime + " second(s)";
+document.getElementById("strikeLimit").innerHTML =
+  strikeLimit + " strikes you're out!";
+document.getElementById("timeLimit").innerHTML =
+  "Time Limit: " + formatTime(timeLeft);
+
+document.getElementById("currentLength").innerHTML =
+  "Pattern Length: " + lengthOfPattern;
+
+//  --------**END OF DESCRIBED CODE BLOCK**--------
+
+function toggleTheme() {
+  document.getElementById("strike").classList.toggle("dark-mode");
+  document.getElementById("body").classList.toggle("dark-mode");
+  document.getElementById("heading").classList.toggle("dark-mode");
+  document.getElementById("paragraph").classList.toggle("dark-mode");
+  document.getElementById("timerFormat").classList.toggle("dark-mode");
+  document.getElementById("bottomMenuBar").classList.toggle("dark-mode");
+  document.getElementById("screenDirection").classList.toggle("dark-mode");
+  document.getElementById("home-section").classList.toggle("dark-mode");
+}
 
 //Gets value from user textbox to keep track of the length the user wants to set
 function getLengthOfPattern() {
   lengthOfPattern = document.getElementById("patternlength").value;
   document.getElementById("currentLength").innerHTML =
-    "Current Length: " + lengthOfPattern;
+    "Pattern Length: " + lengthOfPattern;
 
-  console.log(lengthOfPattern);
+  console.log("Length of pattern: " + lengthOfPattern);
 }
 
+//  --------**END OF FUNCTION**--------
+
+//Gets value from user textbox to set clueHoldTime to user's desired value
+function getClueHoldTime() {
+  clueHoldTime = document.getElementById("clueHoldTime").value * 1000;
+  displayClueHoldTime = document.getElementById("clueHoldTime").value;
+
+  if (clueHoldTime == 0) {
+    clueHoldTime = 3500;
+    displayClueHoldTime = 3.5;
+  }
+
+  document.getElementById("currentHoldTime").innerHTML =
+    "Current Clue Length: " + displayClueHoldTime + " Second(s)";
+
+  console.log("clueHoldTime: " + clueHoldTime + " milliseconds");
+}
+//  --------**END OF getClueHoldTime() FUNCTION**--------
+
+//Gets value from user textbox to set the +/- of clueHoldTime each turn to user's desired value
+
+function getChangeHoldTime() {
+  changeHoldTimeBy = document.getElementById("changeHoldTime").value * 1000;
+  displayChangeHoldTimeBy = document.getElementById("changeHoldTime").value;
+
+  document.getElementById("clueLengthHoldTime+/-").innerHTML =
+    "Change Clue Length Each Turn by: " +
+    displayChangeHoldTimeBy +
+    " Second(s)";
+
+  console.log("changeHoldTime: " + changeHoldTimeBy + " milliseconds");
+}
+//  --------**END OF getChangeHoldTime() FUNCTION**--------
+
+//Updates clueHoldTime by +/- the clueHoldTime by the user's desired value
+function updateClueHoldTime() {
+  clueHoldTime += changeHoldTimeBy;
+  displayClueHoldTime += displayChangeHoldTimeBy;
+  displayClueHoldTime = Math.round(100 * displayClueHoldTime) / 100;
+
+  document.getElementById("currentHoldTime").innerHTML =
+    "Current Clue Length: " + displayClueHoldTime + " Second(s)";
+}
+//  --------**END OF updateClueHoldTime() FUNCTION**--------
+
+//Gets value from user textbox to set the amount of strikes in order to lose
+
+function getChangeStrikes() {
+  strikeLimit = document.getElementById("changeStrikes").value;
+  document.getElementById("strikeLimit").innerHTML =
+    strikeLimit + " strikes you're out!";
+}
+
+//Updates the amount of strikes in order to lose by the user's desired value
+function getChangeTime() {
+  timeLimit = document.getElementById("changeTime").value;
+  document.getElementById("timeLimit").innerHTML =
+    "Time Limit: " + formatTime(timeLeft);
+  document.getElementById("timerFormat").innerHTML = formatTime(timeLeft);
+  timeLeft = timeLimit;
+}
+
+function resetSettings() {
+  document.getElementById("patternlength").value = "";
+  document.getElementById("clueHoldTime").value = "";
+  document.getElementById("changeHoldTime").value = "";
+  document.getElementById("changeStrikes").value = "";
+  document.getElementById("changeTime").value = "";
+
+  timeLimit = 20;
+  timeLeft = timeLimit;
+  lengthOfPattern = 10;
+  clueHoldTime = 3500; //how long to hold each clue's light/sound
+  displayClueHoldTime = 3.5;
+  changeHoldTimeBy = -50;
+  displayChangeHoldTimeBy = -0.05;
+  strikes = 0;
+  strikeLimit = 3;
+
+  document.getElementById("currentLength").innerHTML =
+    "Current Length: " + lengthOfPattern;
+
+  document.getElementById("currentHoldTime").innerHTML =
+    "Current Clue Length: " + displayClueHoldTime + " Second(s)";
+
+  document.getElementById("clueLengthHoldTime+/-").innerHTML =
+    "Change Clue Length Each Turn by: " +
+    displayChangeHoldTimeBy +
+    " Second(s)";
+
+  document.getElementById("strikeLimit").innerHTML =
+    strikeLimit + " strikes you're out!";
+
+  document.getElementById("timeLimit").innerHTML =
+    "Time Limit: " + formatTime(timeLeft);
+
+  document.getElementById("timerFormat").innerHTML = formatTime(timeLeft);
+
+  document.getElementById("timeLimit").innerHTML =
+    "Time Limit: " + formatTime(timeLeft);
+}
 /* If user selects a clue as the computer gives the clues
 before the timer starts they automatically lose! */
 function doNotCheat() {
@@ -40,6 +169,7 @@ function doNotCheat() {
     cheaterLoseGame();
   }
 }
+//  --------**END OF doNotCheat() FUNCTION**--------
 
 // --------**TIMER FUNCTIONS**--------
 function stopTimer() {
@@ -55,9 +185,8 @@ function startTimer() {
 
     timerInterval = setInterval(function () {
       timePassed = timePassed += 1; //Keeptrack of time passed
-      timeLeft = TIME_LIMIT - timePassed;
-      document.getElementById("timerFormat").innerHTML =
-        "Time: " + formatTime(timeLeft);
+      timeLeft = timeLimit - timePassed;
+      document.getElementById("timerFormat").innerHTML = formatTime(timeLeft);
 
       if (timeLeft < 0) {
         loseGame();
@@ -69,9 +198,8 @@ function resetTimer() {
   stopTimer();
   timerInterval = null;
   timePassed = 0;
-  timeLeft = TIME_LIMIT;
-  document.getElementById("timerFormat").innerHTML =
-    "Time: " + formatTime(timeLeft);
+  timeLeft = timeLimit;
+  document.getElementById("timerFormat").innerHTML = formatTime(timeLeft);
   document.getElementById("screenDirection").innerHTML = "";
 
   timerRunning = false;
@@ -86,6 +214,8 @@ function formatTime(timeLeft) {
 
   return `${0}:${seconds}`;
 }
+//  --------**END OF TIMER FUNCTIONS**--------
+
 // --------**RANDOM SEQUENCE GENERATOR**--------
 function getRandomPattern(min, max) {
   min = Math.ceil(min);
@@ -96,17 +226,18 @@ function getRandomPattern(min, max) {
   }
   console.log(pattern);
 }
+// --------**END OF RANDOM SEQUENCE GENERATOR**--------
 
 // --------**START/STOP GAME FUNCTIONS**--------
 
 function startGame() {
   //initialize game variables
   strikes = 0;
-  clueHoldTime = 3500;
   progress = 0;
   gamePlaying = true;
-  //swap the Start and Stop buttons
   pattern = [];
+
+  //swap the Start and Stop buttons
   document.getElementById("startBtn").classList.add("hidden");
   document.getElementById("stopBtn").classList.remove("hidden");
   getRandomPattern(1, 6);
@@ -116,12 +247,15 @@ function startGame() {
 function stopGame() {
   gamePlaying = false;
   strikes = 0;
+  getClueHoldTime();
 
   //swap the Start and Stop buttons
   document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
   document.getElementById("startBtn").classList.remove("hidden");
   document.getElementById("stopBtn").classList.add("hidden");
-  document.getElementById("screenDirection").innerHTML = "";
+  if ((gamePlaying = false)) {
+    document.getElementById("screenDirection").innerHTML = "";
+  }
 
   for (let l = 1; l < 7; l++) {
     removeImage(l);
@@ -131,28 +265,32 @@ function stopGame() {
 
   resetTimer();
 }
+// --------**END OF START/STOP GAME FUNCTIONS**--------
 
 // --------**LIGHT BUTTON WHEN PLAYING FUNCTION**--------
 function lightButton(btn) {
   document.getElementById("button" + btn).classList.add("lit");
 }
+// --------**END OF FUNCTION**--------
 
 // --------**STOP ALL VISUAL AND AUDIO CLUES FUNCTION**--------
-
 function clearButton(btn) {
   document.getElementById("button" + btn).classList.remove("lit");
   pauseMyAudio(btn);
   removeImage(btn);
 }
+// --------**END OF FUNCTION**--------
 
 // --------**PLAY CLUES FUNCTIONS**--------
-
 function playSingleClue(btn) {
   if (gamePlaying) {
     lightButton(btn);
     playMyAudio(btn, clueHoldTime);
     setTimeout(clearButton, clueHoldTime, btn);
     displayImage(btn);
+  }
+  if (gamePlaying == true) {
+    document.getElementById("screenDirection").innerHTML = "Playing Pattern!"; //DISPLAY "PlAYING PATTERN" TO THE USER
   }
 }
 
@@ -162,17 +300,16 @@ function playClueSequence() {
   let delay = NEXT_CLUE_WAIT_TIME; //set delay to initial wait time
   for (let i = 0; i <= progress; i++) {
     // for each clue that is revealed so far
-
-    clueHoldTime -= 50;
     console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
     setTimeout(playSingleClue, delay, pattern[i]); // set a timeout to play that clue
     delay += clueHoldTime;
     delay += CLUE_PAUSE_TIME;
-    console.log("clueHoldTime: " + clueHoldTime);
   }
-
+  console.log("clueHoldTime: " + clueHoldTime);
   setTimeout(startTimer, delay);
 }
+// --------**END OF PLAY CLUES FUNCTIONS**--------
+
 // --------**STOP GAME FUNCTIONS**--------
 function cheaterLoseGame() {
   stopGame();
@@ -188,6 +325,7 @@ function winGame() {
   stopGame();
   alert("Game Over. You won! 😃");
 }
+// --------**END OF STOP GAME FUNCTIONS**--------
 
 // --------**GAME LOGIC FUNCTION**--------
 function guess(btn) {
@@ -206,6 +344,7 @@ function guess(btn) {
       } else {
         //Pattern correct. Add next segment
         progress++;
+        updateClueHoldTime();
         playClueSequence();
         resetTimer();
       }
@@ -217,13 +356,14 @@ function guess(btn) {
     strikes++;
     document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
   }
-  if (strikes == 3) {
+  if (strikes == strikeLimit) {
     document.getElementById("strikes").innerHTML = "Strikes: " + strikes;
     //Guess was incorrect
     //GAME OVER: LOSE!
     loseGame();
   }
 }
+// --------**END OF GAME LOGIC FUNCTION**--------
 
 // --------**IMAGE FUNCTIONS**--------
 
@@ -275,6 +415,7 @@ function removeImage(btn) {
 
   element.classList.add("hiddenImage");
 }
+// --------**END OF IMAGE FUNCTIONS**--------
 
 // --------**AUDIO FUNCTIONS**--------
 
@@ -334,37 +475,13 @@ function pauseMyAudio(btn) {
   pauseAudio.pause();
 }
 
-// Sound Synthesis Functions
-/*const freqMap = {
-  1: 261.6,
-  2: 329.6,
-  3: 392,
-  4: 466.2,
-  5: 496.7,
-  6: 596.5,
-};
-function playTone(btn, len) {
-  o.frequency.value = freqMap[btn];
-  g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
-  context.resume();
-  tonePlaying = true;
-  setTimeout(function () {
-    stopTone();
-  }, len);
-}
-function startTone(btn) {
-  if (!tonePlaying) {
-    context.resume();
-    o.frequency.value = freqMap[btn];
-    g.gain.setTargetAtTime(volume, context.currentTime + 0.05, 0.025);
-    context.resume();
-    tonePlaying = true;
-  }
-}*/
 function stopTone() {
   g.gain.setTargetAtTime(0, context.currentTime + 0.05, 0.025);
   tonePlaying = false;
 }
+// --------**END OF AUDIO FUNCTIONS**--------
+
+// --------**OTHER AUDIO VARIABLES AND AUDIO CODE**--------
 
 // Page Initialization
 // Init Sound Synthesizer
@@ -376,3 +493,4 @@ g.connect(context.destination);
 g.gain.setValueAtTime(0, context.currentTime);
 o.connect(g);
 o.start(0);
+// --------**END OF OTHER AUDIO VARIABLES AND AUDIO CODE**--------
